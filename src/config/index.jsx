d253b6795e985217f3ba4c775d0d826881ca26e0
kyruodioso <kanu_nirvana@hotmail.com>
2024-04-26 17:18:22 -0300

    add pages

--- src/config/index.jsx
+++ src/config/index.jsx
@@ -1,8 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import NotFound from "../pages/NotFound";
 import Route from "../layout/Route"
-import Home from "../pages/Home";
-import Contact from "../pages/Contact";
+import Home from "../pages/home/Home";
+import Contact from "../pages/contact/Contact";
 
 export const router=createBrowserRouter([
     {
---
+++ src/pages/properties/Peroperties.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Peroperties = () => {
+  return (
+    <div>Peroperties</div>
+  )
+}
+
+export default Peroperties
